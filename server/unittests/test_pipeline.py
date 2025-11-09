58918fa6e72630a815e72f01c87a4b486dbdcd4d
Leevi Salonen <102955386+leesalon@users.noreply.github.com>
2025-11-09 20:08:20 +0200

    Add boilerplate game server unittest to check the pipeline is working correctly

---
+++ server/unittests/test_pipeline.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestTemplateForPipeline(unittest.TestCase):
+    """Simple boilerplate class for unittests, intended to check
+that the pipeline is indeed working."""
+
+    def test_always_true(self) -> bool:
+        self.assertTrue(True)
+
