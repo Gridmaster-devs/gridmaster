4c46094dcddc8851c6624847d1b9fa27a531e399
AmethystQuasar <isla.usvakangas@gmail.com>
2025-11-20 15:29:54 +0200

    Clean up editor panel script and exports
    Made editor panel's inspector view cleaner and added the functionality to change the title of the panel from the inspector, defaulting to the panel node's name if one hasn't been set by the user.

--- grid-master-app/editors/common/editor_panel/content_manager.gd
+++ grid-master-app/editors/common/editor_panel/content_manager.gd
@@ -1,19 +1,40 @@
 @tool
 extends PanelContainer
+
+@export var title : String
+
+@export_category("Contents")
 @export_group("Sections")
 @export var section_headers : Array[String]
-@export_tool_button("Update", "Callable") var update_action : Callable = update_headers
+@export_group("")
 
+@export_category("Update")
+@export_tool_button("Update Panel", "Callable") var update_action : Callable = update_panel
+
+func update_panel():
+	if Engine.is_editor_hint():
+		update_name()
+		update_headers()
+
+func update_name():
+	if Engine.is_editor_hint():
+		var top_label : Label = get_node("TopVBox/TopLabel")
+		if title.is_empty():
+			print("Empty title")
+			title = self.name
+		print("Title is " + title)
+		top_label.text = title
+	
 
 func update_headers():
 	if Engine.is_editor_hint():
-		var content_box = get_node("TopVBox/ScrollContainer/ContentsVBox")
+		var content_box : VBoxContainer = get_node("TopVBox/ScrollContainer/ContentsVBox")
 		for child in content_box.get_children():
 			content_box.remove_child(child)
-		for title in section_headers:
+		for h in section_headers:
 			var label = Label.new()
-			label.set_name(title)
-			label.text = title
+			label.set_name(h)
+			label.text = h
 			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 			label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 			content_box.add_child(label, true)
