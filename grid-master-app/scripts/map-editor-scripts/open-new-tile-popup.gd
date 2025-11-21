74036c03cec9f0ba4b9bf8341f5be828f7920dac
Aaro <aaro.lehtinen@aalto.fi>
2025-11-21 14:57:52 +0200

    fixed some folders and naming to match main

--- grid-master-app/scripts/map-editor-scripts/open-new-tile-popup.gd
+++ grid-master-app/scripts/map-editor-scripts/open-new-tile-popup.gd
@@ -11,6 +11,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _on_Button_pressed():
-	var new_tile_popup = preload("res://scenes/map-editor-scenes/create_new_tile.tscn").instantiate()
+	var new_tile_popup = preload("res://editors/map_editor/create_new_tile.tscn").instantiate()
 	get_tree().get_root().add_child(new_tile_popup)
 	new_tile_popup.name = "create_new_tile_popup"
