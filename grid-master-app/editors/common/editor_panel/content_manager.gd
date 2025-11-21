ac2556ff30f122510e8fbe86272f57505a13d5b5
AmethystQuasar <isla.usvakangas@gmail.com>
2025-11-21 12:54:19 +0200

    Provide usable editor panel and panel item
    Editor panel scene should now work fluently with panel item. There seems to be some small bug in removing the items, bit at least in this phase it shouldn't matter much. Does need o be looked into though.

--- grid-master-app/editors/common/editor_panel/content_manager.gd
+++ grid-master-app/editors/common/editor_panel/content_manager.gd
@@ -11,6 +11,8 @@ extends PanelContainer
 @export_category("Update")
 @export_tool_button("Update Panel", "Callable") var update_action : Callable = update_panel
 
+var sub_sections : Dictionary[String, Node] = {}
+
 func update_panel():
 	if Engine.is_editor_hint():
 		update_name()
@@ -28,14 +30,26 @@ func update_name():
 func update_headers():
 	if Engine.is_editor_hint():
 		var content_box : VBoxContainer = get_node("TopVBox/ScrollContainer/ContentsVBox")
-		for child in content_box.get_children():
-			content_box.remove_child(child)
+		var i = 0
 		for h in section_headers:
-			var label = Label.new()
-			label.set_name(h)
-			label.text = h
-			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-			label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-			content_box.add_child(label, true)
-			label.owner = get_tree().edited_scene_root
-			label.theme_type_variation = "SectionLabel"
+			var section : Node = sub_sections.get(h, VBoxContainer.new())
+			if section.get_parent() == content_box:
+				content_box.move_child(section, i)
+			else:
+				var label = Label.new()
+				section.set_name(h)
+				label.text = h
+				label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+				label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+				section.add_child(label, true)
+				content_box.add_child(section, true)
+				section.owner = get_tree().edited_scene_root
+				label.owner = get_tree().edited_scene_root
+				label.theme_type_variation = "SectionLabel"
+				content_box.move_child(section, i)
+				sub_sections.get_or_add(h, section)
+			i += 1
+		for over_max in range(i, content_box.get_child_count()):
+			var removed = content_box.get_child(over_max)
+			sub_sections.erase(removed.name)
+			content_box.remove_child(removed)
