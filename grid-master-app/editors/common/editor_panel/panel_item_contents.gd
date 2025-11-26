4507f2d24163c0f456708c849961e8b95278a3f6
Artemis Paju <valentinepaju@gmail.com>
2025-11-27 00:48:11 +0200

    cleaned up the code and added spinbox type panelitem

--- grid-master-app/editors/common/editor_panel/panel_item_contents.gd
+++ grid-master-app/editors/common/editor_panel/panel_item_contents.gd
@@ -1,13 +1,16 @@
 @tool
 class_name PanelItem
-extends VBoxContainer
+extends HBoxContainer
+
+
+
+enum PanelType {FIELD, CHECKBOX, DROPDOWN, SPINBOX}
 
 @export var text : String
-@export_enum("Field", "Checkbox", "Dropdown") var type : int
+@export_enum("Field", "Checkbox", "Dropdown", "Spinbox") var type : int
 @export_tool_button("Update Item", "Callable") var update_action = update_item
 
 var items : Array[String] = []
-var label_path = "HBoxContainer/ItemName"
 
 # THIS IS THE ISSUE, GETS RESET WHEN THE SCENE IS NOT LIVE
 # REPLACE THE USAGE OF THIS WITH ABSOLUTE PATHS FOR GETTERS AND SETTERS
@@ -15,32 +18,41 @@ var label_path = "HBoxContainer/ItemName"
 # TO SET VALUES IN EDITOR; THIS IS NOT USELESS, JUST NOT FIT FOR GET_VALUE AND SET_VALUE
 # (the get and set at the bottom relate to godot's internal logic and work fine
 # this problem has to do with the top most functions)
-var value_container_path = ""
+# var value_container_path = ""
 
+
+# The label is always child 0, and the content (field, checkbox, etc) is always child 1
+func get_label():
+	# assert(has_type == true, "the panel item has not been given a type, and thus does not have any children")
+	return get_child(0)
+	
+func get_content():
+	# assert(has_type == true, "the panel item has not been given a type, and thus does not have any children")
+	return get_child(1)
 
 func get_value():
 	match type:
-		0:
-			var node : LineEdit = get_node(value_container_path) as LineEdit
+		PanelType.FIELD:
+			var node : LineEdit = get_content() as LineEdit
 			return node.text
-		1:
-			var node : CheckBox = get_node(value_container_path) as CheckBox
+		PanelType.CHECKBOX:
+			var node : CheckBox = get_content() as CheckBox
 			return node.is_pressed()
-		2:
-			var node : OptionButton = get_node(value_container_path) as OptionButton
+		PanelType.SPINBOX:
+			var node : OptionButton = get_content() as OptionButton
 			var index = node.selected
 			return items[index]
 			
 func set_value(val): 
 	match type:
-		0:
-			var node : LineEdit = get_node(value_container_path) as LineEdit
+		PanelType.FIELD:
+			var node : LineEdit = get_content() as LineEdit
 			node.text = val
-		1:
-			var node : CheckBox = get_node(value_container_path) as CheckBox
+		PanelType.CHECKBOX:
+			var node : CheckBox = get_content() as CheckBox
 			node.set_pressed(val)
-		2:
-			var node : OptionButton = get_node(value_container_path) as OptionButton
+		PanelType.DROPDOWN:
+			var node : OptionButton = get_content() as OptionButton
 			node.selected = val
 
 func update_item():
@@ -55,22 +67,24 @@ func update_type():
 		for child in self.get_children():
 			self.remove_child(child)
 		match type:
-			0:
+			PanelType.FIELD:
 				make_field_item()
-			1:
+			PanelType.CHECKBOX:
 				make_checkbox_item()
-			2:
+			PanelType.DROPDOWN:
 				make_dropdown_item()
+			PanelType.SPINBOX:
+				make_spinbox_item()
 
 func update_text(): 
 	if text.is_empty():
 		text = self.name
-	get_node(label_path).text = text
+	get_label().text = text
 	self.name = text
 
 func update_dropdown():
 	if type == 2:
-		var dropdown = get_node(value_container_path) as OptionButton
+		var dropdown = get_child(1) as OptionButton
 		for i in dropdown.item_count:
 			dropdown.remove_item(i)
 		for item in items:
@@ -78,84 +92,95 @@ func update_dropdown():
 	
 func make_field_item():
 	var root = get_tree().edited_scene_root
-	var container = HBoxContainer.new()
 	var label = Label.new()
 	var line_edit = LineEdit.new()
 	
-	self.add_child(container, true)
-	container.add_child(label, true)
-	container.add_child(line_edit, true)
-	
-	container.owner = root
+	self.add_child(label, true)
+	self.move_child(label, 0)
+	self.add_child(line_edit, true)
+	self.move_child(line_edit, 1)
+
 	label.owner = root
 	line_edit.owner = root
 	
-	container.set_anchors_preset(PRESET_HCENTER_WIDE, true)
-	container.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	label_path = label.get_path()
-	value_container_path = line_edit.get_path()
+	self.set_anchors_preset(PRESET_HCENTER_WIDE, true)
+	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	label.size_flags_horizontal = SIZE_EXPAND_FILL
 	line_edit.size_flags_horizontal = SIZE_EXPAND_FILL
 	line_edit.editable = true
 	
 func make_checkbox_item(): 
 	var root = get_tree().edited_scene_root
-	var container = HBoxContainer.new()
 	var label = Label.new()
 	var checkbox = CheckBox.new()
 	
-	self.add_child(container, true)
-	container.add_child(label, true)
-	container.add_child(checkbox, true)
+	self.add_child(label, true)
+	self.move_child(label, 0)
+	self.add_child(checkbox, true)
+	self.move_child(checkbox, 1)
 	
-	container.owner = root
 	label.owner = root
 	checkbox.owner = root
 	
-	container.set_anchors_preset(PRESET_HCENTER_WIDE, true)
-	container.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	label_path = label.get_path()
-	value_container_path = checkbox.get_path()
+	self.set_anchors_preset(PRESET_HCENTER_WIDE, true)
+	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	label.size_flags_horizontal = SIZE_EXPAND_FILL
 	checkbox.size_flags_horizontal = SIZE_EXPAND_FILL
 	checkbox.set_disabled(false)
 	
 func make_dropdown_item():
 	var root = get_tree().edited_scene_root
-	var container = HBoxContainer.new()
 	var label = Label.new()
 	var dropdown = OptionButton.new()
 	
-	self.add_child(container, true)
-	container.add_child(label, true)
-	container.add_child(dropdown, true)
+	self.add_child(label, true)
+	self.move_child(label, 0)
+	self.add_child(dropdown, true)
+	self.move_child(dropdown, 1)
 	
-	container.owner = root
 	label.owner = root
 	dropdown.owner = root
 	
-	container.set_anchors_preset(PRESET_HCENTER_WIDE, true)
-	container.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	label_path = label.get_path()
-	value_container_path = dropdown.get_path()
+	self.set_anchors_preset(PRESET_HCENTER_WIDE, true)
+	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	label.size_flags_horizontal = SIZE_EXPAND_FILL
 	dropdown.size_flags_horizontal = SIZE_EXPAND_FILL
 	dropdown.set_disabled(false)
+	
+func make_spinbox_item(): 
+	var root = get_tree().edited_scene_root
+	var label = Label.new()
+	var spinbox = SpinBox.new()
+	
+	self.add_child(label, true)
+	self.move_child(label, 0)
+	self.add_child(spinbox, true)
+	self.move_child(spinbox, 1)
+	
+	label.owner = root
+	spinbox.owner = root
+	
+	self.set_anchors_preset(PRESET_HCENTER_WIDE, true)
+	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	label.size_flags_horizontal = SIZE_EXPAND_FILL
+	spinbox.size_flags_horizontal = SIZE_EXPAND_FILL
 
 func _get_property_list():
 	var properties = []
 	match type:
-		0:
+		PanelType.FIELD:
 			pass
-		1: 
+		PanelType.CHECKBOX: 
 			pass
-		2:
+		PanelType.DROPDOWN:
 			properties.append({
 				"name": "items",
 				"type": TYPE_ARRAY,
 				"hint": PROPERTY_HINT_ARRAY_TYPE,
 				"hint_string": "%d/%d:" % [TYPE_STRING, PROPERTY_HINT_NONE]
 			})
+		PanelType.SPINBOX:
+			pass
 	return properties
 	
 func _get(property):
