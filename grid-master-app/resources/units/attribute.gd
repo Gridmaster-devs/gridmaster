92f89654b6f3350d3dc1c6ad385a14c16e56dc3f
valentinep <topias.paju@aalto.fi>
2025-11-27 00:08:34 +0200

    added classes for saving

---
+++ grid-master-app/resources/units/attribute.gd
@@ -0,0 +1,28 @@
+class_name Attribute
+
+enum Type {STRING, INT, FLOAT}
+
+var name : String
+var type : Type
+var value
+
+# creates a new instance of Attribute
+# _p here means parameter, distinguishing it from the other variables
+static func create(name_p : String, value_p):
+	var attribute = Attribute.new()
+	attribute.name = name_p
+	attribute.value = value_p
+	
+	if (value_p is int):
+		attribute.type = Type.INT
+	elif (value_p is String):
+		attribute.type = Type.STRING
+	elif (value_p is float):
+		attribute.type = Type.FLOAT
+	
+	assert(attribute.type != null, "The value of the attribute must be an int, string, or float")
+	return attribute
+
+# debugging, prints all there is to the Attribute
+func print_all():
+	print("Attribute: " + str(name) + ", value: " + str(value) + "\n")
---
+++ grid-master-app/resources/units/unit_resource_2.gd
@@ -0,0 +1,18 @@
+class_name UnitResource2
+extends Resource
+
+# holds string : Attribute pairs
+var attributes : Dictionary
+
+func set_attribute(name_p : String, value_p):
+	#TODO: Probably need to check for null values
+	var attribute = Attribute.create(name_p, value_p)
+	attributes[name_p] = attribute
+
+func get_attribute(name_p : String):
+	return attributes[name_p]
+	
+# debugging
+func print_all():
+	for i in attributes.values():
+		i.print_all()
