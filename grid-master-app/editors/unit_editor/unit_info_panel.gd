6e45e5d836ff14cb99fcd6bc4c3f2f500a59cd4e
valentinep <topias.paju@aalto.fi>
2025-12-01 18:42:11 +0200

    unit names and descriptions work fine

---
+++ grid-master-app/editors/unit_editor/unit_info_panel.gd
@@ -0,0 +1,38 @@
+class_name UnitInfoPanel
+extends PanelContainer
+
+@export var unit_editor : UnitEditor
+@onready var save_button = $TopVBox/PanelContainer/ContentsVBox/HBoxContainer/VBoxContainer/Buttons/SaveButton
+@onready var load_button = $TopVBox/PanelContainer/ContentsVBox/HBoxContainer/VBoxContainer/Buttons/LoadButton
+@onready var name_line = $TopVBox/PanelContainer/ContentsVBox/NameLine
+@onready var description_box = $TopVBox/PanelContainer/ContentsVBox/HBoxContainer/ScrollContainer/Description
+
+func save_to_resource(unit_resource : UnitResourceDict):
+	if (unit_resource != null):
+		var unit_name = name_line.text
+		if (unit_name == ""):
+			unit_resource.set_attribute("name", "unnamed unit")
+		else:
+			unit_resource.set_attribute("name", unit_name)
+			
+		var unit_description = description_box.text
+		unit_resource.set_attribute("description", unit_description)
+
+func load_from_resource(unit_resource : UnitResourceDict):
+	if (unit_resource != null):
+		var unit_name = unit_resource.get_attribute("name")
+		if (unit_name == "" or unit_name == null):
+			name_line.text = "unnamed unit"
+		else:
+			name_line.text = unit_name
+			
+		var unit_description = unit_resource.get_attribute("description")
+		if (unit_description == null):
+			description_box.text = ""
+		else:
+			description_box.text = unit_description
+		
+func _ready():
+	if (unit_editor != null):
+		unit_editor.save_to_resource.connect(save_to_resource)
+		unit_editor.load_from_resource.connect(load_from_resource)
