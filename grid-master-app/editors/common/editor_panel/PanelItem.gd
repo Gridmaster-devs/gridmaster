0217c385d55ea4cf59836c3de3829dae1ca5313b
Artemis Paju <valentinepaju@gmail.com>
2026-01-12 20:42:12 +0200

    progress on fixing panel item mistake

--- grid-master-app/editors/common/editor_panel/PanelItem.gd
+++ grid-master-app/editors/common/editor_panel/PanelItem.gd
@@ -1,11 +1,8 @@
 @tool
-class_name UnitPanelItem
+class_name PanelItem
 extends HBoxContainer
 
 enum PanelType {FIELD, CHECKBOX, DROPDOWN, SPINBOX}
-
-# this is the name of the attribute that will be saved in the dictionary
-@export var attribute_name : String
 
 # the text that will be shown to the user, explains the attribute
 @export var text : String
@@ -19,8 +16,6 @@ enum PanelType {FIELD, CHECKBOX, DROPDOWN, SPINBOX}
 # various flags
 # the resource flag determines whether the attribute counts as a resource, and thus
 # whether it shows up in places for resources, like certain actions
-@export_flags ("Resource") var flags : int = 0
-var root_editor : UnitEditor
 
 var items : Array[String] = [] # Array for the dropdown menu option
 
@@ -47,22 +42,6 @@ func reset():
 		PanelType.SPINBOX:
 			var node : SpinBox = get_content() as SpinBox
 			node.value = 0
-
-func save_to_unit_resource(resource_p : UnitResourceDict):
-	if (resource_p != null):
-		resource_p.set_attribute(attribute_name, get_value())
-	
-func load_from_unit_resource(resource_p : UnitResourceDict):
-	if (resource_p != null):
-		var value = resource_p.get_attribute_value(attribute_name)
-		if (value != null):
-			# this assumes that the type of the attribute is the same in the dictionary and in here
-			# this obviously means that if the value type of an attribute changes between versions their units
-			# won't be compatible with each other
-			set_value(value) 
-			return
-
-	reset()
 
 func get_value():
 	match type:
@@ -228,23 +207,6 @@ func _get_property_list():
 			pass
 	return properties
 	
-func _on_save_to_resource(resource : UnitResourceDict):
-	save_to_unit_resource(resource)
-	
-func _on_load_from_resource(resource : UnitResourceDict):
-	load_from_unit_resource(resource)
-	
-# super super jank but I don't know a better way right now
-# some way to do globals would probably be the best way
-func find_root_unit_editor():
-	var cur_node = self.get_parent()
-	while true:
-		if cur_node is UnitEditor:
-			root_editor = cur_node
-		elif cur_node == null:
-			break
-		cur_node = cur_node.get_parent()
-
 # solely for debugging
 func test_parent():
 	var parent = self.get_parent()
@@ -252,24 +214,6 @@ func test_parent():
 		print("name: " + self.name + ", parent: " + str(parent))
 		parent = parent.get_parent()
 		
-# checks whether the resource flag is on
-func resource_flag() -> bool:
-	if ((flags & 1) > 0):
-		return true
-	else:
-		return false
-
-# adds itself to the resources if the resource flag is set
-func update_resources(resources : Array[String]):
-	if (resource_flag()):
-		resources.append(attribute_name)
-	
+		
 func _ready():
-	if !Engine.is_editor_hint():
-		find_root_unit_editor()
-		if root_editor != null:
-			root_editor.save_to_resource.connect(save_to_unit_resource)
-			root_editor.load_from_resource.connect(load_from_unit_resource)
-			root_editor.reset.connect(reset)
-			root_editor.update_resources.connect(update_resources)
 	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
