60045da646f4a4b80790d3acf31bba149e608dc2
Artemis Paju <valentinepaju@gmail.com>
2026-01-14 15:41:54 +0200

    more progress on internals

--- game-executioner/main/UnitType.gd
+++ game-executioner/main/UnitType.gd
@@ -4,17 +4,18 @@ class_name UnitType
 enum MOVEMENT_TYPE{WALKING = 0, TIRES = 1, TRACKS = 2, HOVERING = 3, TELEPORTING = 4, WATER = 5, CUSTOM = -1}
 enum CAPTURABLE_TYPE{NO = 0, YES = 1}
 
+var name : String
+var description : String
 var max_hp : int
 var attack : int
 var speed : int
 var capturable : CAPTURABLE_TYPE
 var movement_type : MOVEMENT_TYPE
+var vision_range : int
+var perception : int
+var armor : int
+var dodge : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
