9cb8321a8ad79f2c3b7802f9aa80caa4b33e3fad
Artemis Paju <valentinepaju@gmail.com>
2026-01-30 13:35:10 +0200

    Added file access web plugin

--- grid-master-app/common/file_upload_download/resource_file_manager.gd
+++ grid-master-app/common/file_upload_download/resource_file_manager.gd
@@ -8,15 +8,18 @@ var process_ongoing : bool = false ## Whether we have a file dialog open right n
 var current_dialog : FileDialog ## Reference to the current file dialog if there is one so it can be freed
 var current_resource : Resource ## Reference to the current resource that is being downloaded
 
+var current_faw : FileAccessWeb ## Reference to the current file access web object for uploading
+var current_local_upload_signal : Signal
+
 
 ## Prompts the user for a download of a file with a specific content and filename
 func download_resource(resource : Resource, default_filename : String) -> void:
-	# If build is running on the web
+	# If build is running in the browser
 	if (OS.has_feature("web") == true):
 		var data : PackedByteArray = var_to_bytes_with_objects(resource)
 		JavaScriptBridge.download_buffer(data, default_filename)
 
-	# If build is not running on the web
+	# If build is not running in the browser
 	else:
 		# If we already have a file dialog window open, we shouldn't open
 		# another one
@@ -45,6 +48,36 @@ func _finish_local_download(path : String) -> void:
 	ResourceSaver.save(current_resource, path)
 	_process_done()
 
+
+func initialize_upload() -> Variant:
+	# If build is running in the browser
+	if (OS.has_feature("web")):
+		var file_access_web = FileAccessWeb.new()
+		current_faw = file_access_web
+		return current_faw.loaded
+
+
+	# If build is not running in the browser
+	else:
+		if (process_ongoing == true): return null
+		process_ongoing = true
+		
+		var dialog := FileDialog.new()
+		
+		dialog.access = FileDialog.ACCESS_FILESYSTEM # User can save to anywhere in the filesystem
+		dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE # User is prompted to save a file
+		dialog.display_mode = FileDialog.DISPLAY_LIST # Files will be shown as a list and not as thumbnails
+		dialog.add_filter(ALLOWED_FILETYPES, "Resource files") # Filter how the file can be saved
+		
+		dialog.file_selected.connect(_finish_local_upload) # If the user selects a file
+		dialog.close_requested.connect(_process_done) # If the user closes the dialog window
+		current_dialog = dialog
+		self.add_child(dialog) # I don't know if this is even necessary to show the dialog box to the user?
+		dialog.show()
+
+
+func _finish_local_upload(path : String) -> void:
+	pass
 
 ## Frees the dialog box
 ##
