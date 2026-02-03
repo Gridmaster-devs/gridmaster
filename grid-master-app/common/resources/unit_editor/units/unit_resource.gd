7c5ad23849fe19b75153c846642b5ac8b5c26ca4
Leevi Salonen <102955386+leesalon@users.noreply.github.com>
2026-02-03 17:54:51 +0200

    Add return type hints and remove / cleanup some parts of the code.

--- grid-master-app/common/resources/unit_editor/units/unit_resource.gd
+++ grid-master-app/common/resources/unit_editor/units/unit_resource.gd
@@ -8,37 +8,32 @@ extends Resource
 
 
 ## takes an attribute of a certain name and puts its value in the dictionary
-func set_attribute(name_p : String, value_p):
+func set_attribute(name_p : String, value_p: Variant) -> void:
 	#TODO: Probably need to check for null values
 	if (name_p != null and value_p != null):
 		var attribute = Attribute.create(name_p, value_p)
 		attributes[name_p] = attribute
 
 
-## checks whether and attribute exists in the dictionary and returns it if it does
-func get_attribute(name_p : String):
-	var attribute : Attribute = attributes.get(name_p)
-	if (attribute != null):
-		return attribute
-	else:
-		return null
+## returns the attribute value or null if name_p isn't in the dictionary keys
+func get_attribute(name_p : String) -> Variant:
+	return attributes.get(name_p)
 
 
 ## returns the value of the given attribute
-func get_attribute_value(name_p : String):
+func get_attribute_value(name_p : String) -> Variant:
 	var attribute : Attribute = attributes.get(name_p)
-	if (attribute == null): return null
-	if (attribute.get_attribute_value() == null): return null
-	return attribute.get_attribute_value()
+	if (attribute): return attribute.get_attribute_value()
+	return null
 
 
 ## takes a an array of actions and saves it in the resource
-func save_actions(action_array : Array[Action]):
+func save_actions(action_array : Array[Action]) -> void:
 	actions = action_array
 
 
 ## returns the actions saved in the resource
-func load_actions():
+func load_actions() -> Array:
 	return actions
 	
 
@@ -48,6 +43,6 @@ func getAttributes() -> Dictionary:
 
 	
 # debugging
-func print_all():
+func print_all() -> void:
 	for i in attributes.values():
 		i.print_all()
