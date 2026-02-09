a4b2d2183f20a66b24c026d0c5c718ef3ff5c9ec
Artemis Paju <valentinepaju@gmail.com>
2026-02-09 14:08:51 +0200

    added ftm to game executioner

--- grid-master-app/executioner/main/user_interface/UserInterface.gd
+++ grid-master-app/executioner/main/user_interface/UserInterface.gd
@@ -1,18 +1,17 @@
 class_name UserInterface
 extends Control
 
-@onready var load_game_dialog : FileDialog = $"Dialogs/Load game dialog"
+@onready var ftm : FileTransferManager = $Dialogs/FileTransferManager
 var game_master : GameMaster
 
 
 ## Opens the load game dialog
 func openLoadGameDialog() -> void:
-	load_game_dialog.show()
+	ftm.upload_data("*.tres", true)
 
 
 ## Called by the load game dialog
-func loadGameDefinition(path : String):
-	var game_definition : GameDefinitionResource = ResourceLoader.load(path) as GameDefinitionResource
+func loadGameDefinition(game_definition : Resource):
 	assert(game_definition != null, "Invalid game definition in file!")
 	game_master.playerSelectedGameDefinition(game_definition)
 
@@ -24,7 +23,7 @@ func linkGameMaster(game_master_p : GameMaster) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_game_dialog.file_selected.connect(loadGameDefinition)
+	ftm.resource_uploaded.connect(loadGameDefinition)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
