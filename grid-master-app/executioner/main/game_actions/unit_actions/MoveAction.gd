65ae63148d2e05f2b9b276840a73d03b7047bd46
valentinep <topias.paju@aalto.fi>
2026-02-11 22:28:48 +0200

    added execution to the movement action

--- grid-master-app/executioner/main/game_actions/unit_actions/MoveAction.gd
+++ grid-master-app/executioner/main/game_actions/unit_actions/MoveAction.gd
@@ -1,8 +1,15 @@
 class_name MoveAction
 extends UnitAction
+## An action that represents a unit moving from place A to place B
 
-var target_position : Vector2i ## Where the player wants to move the unit
-var waypoints : Array[Vector2i]
+# Currently only the first and last tile of the path matter,
+# but this could change in the future if we implement, for example,
+# land mines.
+## Path of nodes the unit is set to travel.
+## The first item is always the starting position of the unit,
+## and the last item is always the final position of the unit.
+var path : Array[Vector2i]
+
 
 func execute(game_state : GameState):
-	pass
+	game_state.move_unit(unit_id, path.back())
