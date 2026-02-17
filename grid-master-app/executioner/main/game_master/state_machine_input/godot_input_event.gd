c2e3dbc1a395cbc0cee4e6bedbc5d91de1ecb22f
Artemis Paju <valentinepaju@gmail.com>
2026-02-17 14:06:56 +0200

    progress on game master state machine

---
+++ grid-master-app/executioner/main/game_master/state_machine_input/godot_input_event.gd
@@ -0,0 +1,4 @@
+class_name GodotInputEvent
+extends StateMachineInput
+
+var event : InputEvent
---
+++ grid-master-app/executioner/main/game_master/state_machine_input/state_machine_input.gd
@@ -0,0 +1,4 @@
+@abstract
+class_name StateMachineInput
+## A class that represents an input that the game master can get
+## that is processed by the finite state machine
