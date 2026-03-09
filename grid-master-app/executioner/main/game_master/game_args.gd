8eefab90a90ee6d8ab428311c66772b311691b22
valentinep <topias.paju@aalto.fi>
2026-03-09 17:55:38 +0200

    added variable accessors to unit class, and added hit function

--- grid-master-app/executioner/main/game_master/game_args.gd
+++ grid-master-app/executioner/main/game_master/game_args.gd
@@ -4,10 +4,25 @@ class_name GameArgs
 ## The types of arguments
 enum ArgType {FIGHT_FUNC, DAMAGE_FUNC, HIT_FUNC, UNIT_INITIATIVE_FUNC}
 
-# The fight function must have the signature of (unit1, unit2) -> void
-# It determines how a fight happens
+## What the accuracy, dodge change, etc. are defined up to.
+##
+## Ex. with the value 100, 50 dodge means 50% dodge chance, but
+## with the value 1000, 50 dodge chance means 5% dodge chance.
+const PERCENT_RANGE : int = 100
+
+# The fight function must have the signature (unit1, unit2) -> void
+# It determines how a fight happens and executes it
 # It does not and should not remove units from the map. That is the job
 # of the end turn function in the game master
+
+# The damage function must have the signature (unit) -> int.
+# It determines what damage the unit deals
+
+# The hit function must have the signature (unit1, unit2) -> bool.
+# It determines whether the hit goes through or not
+
+# The unit initiative function must have the signature (unit_array) -> void.
+# It takes in an array of units and sorts them according to some criteria
 
 # Bunch of flags for MoveActions
 const FLAG_STOP_AFTER_FIGHTING = 1
@@ -34,9 +49,13 @@ static func set_move_action_flags(flags : int):
 	else:
 		stop_after_fighting = false
 	
-	
 
-# PUT PROPER FIGHT FUNC HERE
+# Gives a random number between 0 and PERCENT RANGE (inclusive)
+func rand_range_val() -> int:
+	return randi_range(0, PERCENT_RANGE)
+
+
+# TODO: PUT PROPER FIGHT FUNC HERE
 func _gen_default_fight_func() -> void:
 	var fight_func = func(unit1 : Unit, unit2 : Unit):
 		unit1.take_damage(unit2.get_damage())
@@ -51,8 +70,19 @@ func _gen_default_damage_func() -> void:
 	pass
 
 func _gen_default_hit_func() -> void:
-	pass
+	var hit_func = func(unit1 : Unit, unit2 : Unit) -> bool:
+		if (rand_range_val() > unit1.accuracy):
+			# Unit 1 missed
+			return false
+		
+		if (rand_range_val() <= unit2.dodge):
+			# Unit 2 dodged
+			return false
+		
+		# Unit 1 hit the target
+		return true
 
+	args.set(ArgType.HIT_FUNC, hit_func)
 
 func _gen_default_initiative_func() -> void:
 	
