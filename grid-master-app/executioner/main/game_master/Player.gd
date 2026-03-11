792177d4f70ff4d94fc1ecacf7b9689cca3fb73b
valentinep <topias.paju@aalto.fi>
2026-03-11 13:46:36 +0200

    made it so that the player cannot control units that are not theirs

--- grid-master-app/executioner/main/game_master/Player.gd
+++ grid-master-app/executioner/main/game_master/Player.gd
@@ -7,8 +7,10 @@ var player_id : int ## The ID of the player
 var team : Team ## The ID of the team the player is part of
 var computer : bool ## Whether the player is computer or human controlled
 
+
 static func DEBUG_new_player() -> Player:
 	return Player.new("Debug player", -1, Team.DEBUG_new_team(), false)
+
 
 func _init(player_name_p : String, player_id_p : int, team_p : Team, computer_p : bool) -> void:
 	player_name = player_name_p
