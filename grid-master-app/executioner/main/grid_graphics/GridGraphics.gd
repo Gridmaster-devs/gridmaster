186e5e0c6a58248bf9ede6fc31abe8254d6ca270
Artemis Paju <valentinepaju@gmail.com>
2026-03-13 17:53:20 +0200

    removed useless variables from GridGraphics

--- grid-master-app/executioner/main/grid_graphics/GridGraphics.gd
+++ grid-master-app/executioner/main/grid_graphics/GridGraphics.gd
@@ -12,23 +12,12 @@ const EVENT_INPUT_FUNC_NAME : StringName = "handle_input"
 
 const UNIT_CONTAINER : PackedScene = preload("res://executioner/main/grid_graphics/unit_container/unit_container.tscn")
 
-## What state the grid graphics object is in
-# DEFAULT: Only the selected tile hover graphic is drawn
-# UNIT_MOVE: Selected tile and the path from the last waypoint to the cursor is drawn
-enum GraphicsState {DEFAULT, UNIT_MOVE}
 
 @onready var ui_map_grid = $"SubViewportContainer/Grid Graphics Viewport/TileGrid"
 @onready var background_grid = $"SubViewportContainer/Grid Graphics Viewport/BackgroundGrid"
 @onready var grid_graphics_viewport = $"SubViewportContainer/Grid Graphics Viewport"
 @onready var tile_grid : TileMapLayer = $"SubViewportContainer/Grid Graphics Viewport/TileGrid"
 @onready var custom_graphics : CustomGraphics = $"SubViewportContainer/Grid Graphics Viewport/CustomGraphics"
-
-
-var graphics_state : GraphicsState = GraphicsState.DEFAULT
-
-# Variables for UNIT_MOVE
-var possible_movement_tiles : Array[Vector2i]
-var moved_unit_pos : Vector2i
 
 
 var game_master : GameMaster # NOTE: Might be unnecessary when using groups
