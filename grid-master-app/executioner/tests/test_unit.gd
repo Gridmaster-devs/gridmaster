970e6bb1a1957618d6c06893476854a46070beb6
Algebruh3 <pajunenmikael@gmail.com>
2026-03-16 16:42:18 +0200

    Update unit initializations to match current Unit implementation

--- grid-master-app/executioner/tests/test_unit.gd
+++ grid-master-app/executioner/tests/test_unit.gd
@@ -3,7 +3,9 @@ extends GutTest
 # Test initializing a unit
 func test_unit_init():
 	var unit_type: UnitType = UnitType.debugType()
-	var unit: Unit = Unit.new(unit_type, -1, -1, Vector2i(1, 1))
+	var team: Team = Team.new("Test team", -1, Color(0, 0, 0), [])
+	var player: Player = Player.new("Test player", -1, team, false)
+	var unit: Unit = Unit.new(unit_type, -1, player, Vector2i(1, 1))
 
 	unit.initFromUnitType()
 	
