3e9094141253e57ecf2c8d387d70a62f07c01021
pekko <44174921+pekkoeskola@users.noreply.github.com>
2026-04-07 00:59:59 +0300

    Moved players and teams to GameDefinition from GameState

--- grid-master-app/executioner/main/game_master/game_definition.gd
+++ grid-master-app/executioner/main/game_master/game_definition.gd
@@ -12,7 +12,14 @@ var game_name : String:
 		
 var unit_types : Dictionary[int, UnitType] = {} ## All the types of units in the game
 
+var players : Dictionary[int, Player] = {-1 : Player.NEUTRAL_PLAYER} ## All the players in the game
+var teams : Dictionary[int, Team] = {-1 : Team.NEUTRAL_TEAM} ## All the teams in the game
+
 var pathfinder : DijkstraPathfinder ## Dijkstra pathfinder for unit pathing
+
+# These are used for generating team and player ids
+var teams_index = 0 # Count for how many teams there are
+var players_index = 0 # Count for how many players there are
 
 ## Initializes the unit types from a game definition
 func initUnitTypesFromResource(game_definition : GameDefinitionResource, 
@@ -25,6 +32,29 @@ func initUnitTypesFromResource(game_definition : GameDefinitionResource,
 		unit_name_type_dict[ cur_unit_type.unit_name] = type_count
 		type_count += 1
 		
+## Adds a team to the game.
+func add_team(team_name : String, color : Color, team_units : Array[UnitType]) -> int:
+	var team_id : int = get_new_team_id()
+	teams.set(team_id, Team.new(team_name, team_id, color, team_units))
+	return team_id
+
+
+## Adds a player to the game.
+func add_player(player_name : String, team_id : int, computer : bool) -> int:
+	var player_id = get_new_player_id()
+	players.set(player_id, Player.new(player_name, player_id, teams.get(team_id), computer))
+	return player_id
+	
+func get_player_by_id(id: int) -> Player:
+	return players[id]
+
+func get_new_team_id() -> int:
+	teams_index += 1
+	return teams_index
+
+func get_new_player_id() -> int:
+	players_index += 1
+	return players_index
 
 # INFO DEBUG methods
  
