4711caeb870985009f41273e229f07fbe961897f
pekko <44174921+pekkoeskola@users.noreply.github.com>
2026-04-12 01:52:14 +0300

    Removed Unit reference from UnitContainer (it was redundant anyway). Tidied up Gridgraphics by marking more internal methods with an underscore according to convention.

--- grid-master-app/executioner/main/grid_graphics/GridGraphics.gd
+++ grid-master-app/executioner/main/grid_graphics/GridGraphics.gd
@@ -29,25 +29,27 @@ var _grid_height : int
 ## maybe later just have it take the texture from the unit? 
 var active_units: Array[UnitContainer]
 
-
+# TODO this is an anti-pattern IMHO, let the parent class connect the signal instead
+# of doing it in this roundabout way, also remove GameMaster reference
+# and replace with a more elegant solution to query state as needed
 ## Gives this object a reference to the game master, and connects
 ## its signals to functions in this object
 func linkGameMaster(game_master_p : GameMaster):
 	game_master = game_master_p
-	game_master.units_changed.connect(unitsChanged)
+	game_master.units_changed.connect(_unitsChanged)
 
 
 ## Called by the game master at the start
 func initFromGameGrid(game_grid : GameGrid):
 	grid_graphics_viewport.resize(self.size)
-	initTileGrid(game_grid)
-	initBackgroundGrid(game_grid)
-	getUnits()
+	_initTileGrid(game_grid)
+	_initBackgroundGrid(game_grid)
+	_getUnits()
 
 
 ## initializes tile sources for the tileMapLayer from the loaded GameGrid
 ## should only be called from initTileGrid
-func initTileSources(game_grid: GameGrid) -> void: 
+func _initTileSources(game_grid: GameGrid) -> void: 
 	for tile_type in game_grid.strategic_tile_types.values():
 		var source = TileSetAtlasSource.new()
 		source.texture = tile_type.texture
@@ -57,8 +59,8 @@ func initTileSources(game_grid: GameGrid) -> void:
 
 
 ## initializes the map element of the game from data loaded into the game_state
-func initTileGrid(game_grid : GameGrid) -> void: 
-	initTileSources(game_grid)
+func _initTileGrid(game_grid : GameGrid) -> void: 
+	_initTileSources(game_grid)
 	
 	_grid_width = game_grid.getWidth()
 	_grid_height = game_grid.getHeight()
@@ -75,12 +77,12 @@ func gridToScreen(grid_pos: Vector2i) -> Vector2:
 
 
 ## Initializes the background grid
-func initBackgroundGrid(game_grid): 
+func _initBackgroundGrid(game_grid): 
 	background_grid.resize(game_grid.getWidth(), game_grid.getHeight())
 
 
 ## Removes all active units
-func clearUnits():
+func _clearUnits():
 	for i in active_units.size():
 		var unit = active_units.front()
 		active_units.remove_at(0)
@@ -88,11 +90,11 @@ func clearUnits():
 
 
 ## Gets the units from the game master and puts them in the active units array
-func getUnits():
+func _getUnits():
 	if (game_master != null):
 		var units = game_master.getUnits()
 		if (units != null):
-			clearUnits()
+			_clearUnits()
 			for unit : Unit in units:
 				var unit_container : UnitContainer = UNIT_CONTAINER.instantiate()
 				addNewUnitContainer(unit_container)
@@ -107,9 +109,9 @@ func addNewUnitContainer(unit_cont: UnitContainer):
 
 
 ## Called when the Game Master emits the units_changed signal
-func unitsChanged() -> void:
-	clearUnits()
-	getUnits()
+func _unitsChanged() -> void:
+	_clearUnits()
+	_getUnits()
 
 
 ## Returns the coordinates of the tile that 
