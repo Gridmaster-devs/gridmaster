d48d3dcc2b34617955a19cdbddcc4851e39ad62b
Algebruh3 <pajunenmikael@gmail.com>
2026-04-17 19:24:55 +0300

    Add fog of war logic to show visible units
    Adds get_visible_units and is_unit_visible, which add fog of war logic
    and determine, which units are visible to player

--- grid-master-app/executioner/main/game_master/game_data_manager.gd
+++ grid-master-app/executioner/main/game_master/game_data_manager.gd
@@ -35,11 +35,60 @@ func get_game_name() -> String:
 func get_units() -> Dictionary[int, Unit]:
 	return _game_state._units
 
+# Returns all units currently visible from the perspective of the client player
+func get_visible_units() -> Variant:
+	if _game_state == null:
+		return null
+	
+	var visible_units: Array[Unit] = []
+	# Use the pov of the client player
+	var client_player_id: int = _client_attributes.client_player_id
+	var client_player: Player =  _game_definition.players.get(client_player_id)
+	
+	if client_player == null:
+		return []
+	
+	var client_team_id: int = client_player.team.team_id
+	# Goes over each unit in the game and checks whther the client player sees it or not
+	for unit in _game_state.getUnits():
+		var unit_team_id: int = unit.get_team_id()
+		
+		# Always sees units on the same team
+		if unit_team_id == client_team_id:
+			visible_units.append(unit)
+		elif _is_unit_visible(unit, client_player_id):
+			visible_units.append(unit)
+		
+	return visible_units
+
 func get_unit_by_position_nullable(pos: Vector2i) -> Unit:
 	return _game_state.get_unit_by_position_nullable(pos)
 
 func get_grid() -> GameGrid:
 	return _game_definition.grid
+
+# Used for visibility logic. Checks if the target unit is visible to the given player
+## TODO probably should not be here, could be moved to Player if the player knows its own units
+func _is_unit_visible(target_unit: Unit, player_id: int) -> bool:
+	var player: Player = _game_definition.players.get(player_id)
+	
+	if player == null:
+		return false
+	
+	var team_id: int = player.team.team_id
+	
+	# Check visibility against all units owned by the same team
+	for unit in _game_state.getUnits():
+		if unit.get_team_id() == team_id:
+			# Compute using Chebyshev distance
+			var dx: int = abs(unit.getPosition().x - target_unit.getPosition().x)
+			var dy: int = abs(unit.getPosition().y - target_unit.getPosition().y)
+			
+			# If the target is within vision range of this unit means it is visible 
+			if max(dx, dy) <= unit.vision_range:
+				return true
+		
+	return false
 	
 # ---
 # INFO mutating methods
