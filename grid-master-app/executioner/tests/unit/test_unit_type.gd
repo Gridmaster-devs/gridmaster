3e50a393f4ff6aeba9dc1850dfe44e187e3829b3
Algebruh3 <pajunenmikael@gmail.com>
2026-04-20 12:38:15 +0300

    Reorganize unit tests into own folder

---
+++ grid-master-app/executioner/tests/unit/test_unit_type.gd
@@ -0,0 +1,25 @@
+extends GutTest
+
+var unit_resource: UnitResource
+
+# Initialize unit resource and set its attributes
+func before_each():
+	unit_resource = UnitResource.new()
+	unit_resource.set_attribute("name", "Test Unit")
+	unit_resource.set_attribute("description", "Testing unit")
+	
+	for a : String in UnitType.attribute_conversion_table.keys():
+		unit_resource.set_attribute(a, 1)
+
+# Clear the unit resource
+func after_each():
+	unit_resource = null
+
+# Test initializing UnitType from a UnitResourceDict
+func test_init_from_unit_resource():
+	var unit_type: UnitType = UnitType.initFromUnitResource(unit_resource, -1)
+	
+	for a : String in unit_type.attribute_conversion_table.keys():
+		var attribute_type: UnitType.UNIT_ATTRIBUTE_TYPE = unit_type.attribute_conversion_table.get(a)
+		var attribute_value: Variant = unit_type.attributes.get(attribute_type)
+		assert_eq(unit_resource.get_attribute(a).get_attribute_value(), attribute_value)
