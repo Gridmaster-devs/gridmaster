8e4bdafe91a8dd13cbbe94b5ffefb5928af0078f
Algebruh3 <pajunenmikael@gmail.com>
2026-04-22 17:33:26 +0300

    Add unit count for different UnitTypes

--- grid-master-app/executioner/main/game_master/Player.gd
+++ grid-master-app/executioner/main/game_master/Player.gd
@@ -9,6 +9,18 @@ var player_id : int ## The ID of the player
 var team : Team ## The ID of the team the player is part of
 var computer : bool ## Whether the player is computer or human controlled
 
+# Keeps track of how many of each UnitTypes the player has
+var unit_type_counts: Dictionary[UnitType, int] = {}
+
+
+func increment_unit_count(unit_type: UnitType) -> int:
+	if not unit_type_counts.has(unit_type):
+		unit_type_counts[unit_type] = 1
+	else:
+		unit_type_counts[unit_type] += 1
+	
+	return unit_type_counts[unit_type]
+
 
 static func DEBUG_new_player() -> Player:
 	return Player.new("Debug player", -1, Team.DEBUG_new_team(), false)
