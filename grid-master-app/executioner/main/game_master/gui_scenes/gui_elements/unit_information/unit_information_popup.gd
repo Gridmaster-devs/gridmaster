795bbaa9218a7938b860c065022494a9e91db14b
pekko <44174921+pekkoeskola@users.noreply.github.com>
2026-04-23 13:26:20 +0300

    Hotfix: hp and morale in unit info popup now reflect real values that change during the game

--- grid-master-app/executioner/main/game_master/gui_scenes/gui_elements/unit_information/unit_information_popup.gd
+++ grid-master-app/executioner/main/game_master/gui_scenes/gui_elements/unit_information/unit_information_popup.gd
@@ -7,6 +7,8 @@ extends PopupWindow
 
 var _data_information_item_map: Dictionary[String, UnitInformationItem]
 
+@onready var unit_info_item = preload("res://executioner/main/game_master/gui_scenes/gui_elements/unit_information/unit_information_item.tscn")
+
 func _init() -> void: 
 	super.set_popup_name("unit information popup")
 	super.set_unblocking()
@@ -15,16 +17,31 @@ func _init() -> void:
 
 func set_unit_info(unit: Unit) -> void: 
 	# Add the unit name first
-	var name_item: UnitInformationItem = preload("res://executioner/main/game_master/gui_scenes/gui_elements/unit_information/unit_information_item.tscn").instantiate()
+	var name_item: UnitInformationItem = unit_info_item.instantiate()
 	_contents.add_child(name_item)
 	name_item.set_information("Name", unit.get_unit_name())
 	_data_information_item_map["Name"] = name_item
 	
 	var unit_type_attribute_names = unit.type.attribute_conversion_table.keys()
+	
+	#CAUTION FIXME dumbass hack to get dynamic hp and morale
 	for attribute_name in unit_type_attribute_names:
-		var attribute_key = unit.type.attribute_conversion_table[attribute_name]
-		var attribute_value = unit.type.attributes[attribute_key]
-		var information_item: UnitInformationItem = preload("res://executioner/main/game_master/gui_scenes/gui_elements/unit_information/unit_information_item.tscn").instantiate()
-		_contents.add_child(information_item)
-		information_item.set_information(attribute_name, str(attribute_value))
-		_data_information_item_map[attribute_name] = information_item
+		if attribute_name == "health":
+			var attribute_value = unit.hp
+			var information_item: UnitInformationItem = unit_info_item.instantiate()
+			_contents.add_child(information_item)
+			information_item.set_information(attribute_name, str(attribute_value))
+			_data_information_item_map[attribute_name] = information_item
+		elif attribute_name == "morale":
+			var attribute_value = unit.morale
+			var information_item: UnitInformationItem = unit_info_item.instantiate()
+			_contents.add_child(information_item)
+			information_item.set_information(attribute_name, str(attribute_value))
+			_data_information_item_map[attribute_name] = information_item
+		else:
+			var attribute_key = unit.type.attribute_conversion_table[attribute_name]
+			var attribute_value = unit.type.attributes[attribute_key]
+			var information_item: UnitInformationItem = unit_info_item.instantiate()
+			_contents.add_child(information_item)
+			information_item.set_information(attribute_name, str(attribute_value))
+			_data_information_item_map[attribute_name] = information_item
