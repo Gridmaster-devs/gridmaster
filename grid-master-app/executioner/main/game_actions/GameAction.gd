3fe170fa823a15fa2b4bb6dcd419267a13657772
Algebruh3 <pajunenmikael@gmail.com>
2026-06-11 14:14:22 +0300

    Adds a helper function for actions that take multiple rounds

--- grid-master-app/executioner/main/game_actions/GameAction.gd
+++ grid-master-app/executioner/main/game_actions/GameAction.gd
@@ -8,3 +8,8 @@ var player_id : int ## The player attempting to make the action
 
 ## Produces a deep copy of the object
 @abstract func deep_copy(unit: Unit) -> GameAction
+
+# Determines if the action is interruptible. For example it is false for ProductionAction and others that can
+# take multile rounds to finish
+func is_interruptible() -> bool:
+	return true
